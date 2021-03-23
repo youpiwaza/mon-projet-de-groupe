2b5fe93c85dafd5a4df804ff8934ecac5ff6308b
nacer <nacer.yeles@gmail.com>
2021-03-23 12:04:08 +0100

    toto

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let title2 = document.querySelectorAll("h3");
 let p = document.querySelectorAll("p");
 
 
-title.forEach(e => e.innerHTML = "toto")
+title.forEach(e => e.innerHTML = "toto1")
 title1.forEach(e => e.innerHTML = "toto")
 title2.forEach(e => e.innerHTML = "toto")
 p.forEach(e => e.innerHTML = "toto")
