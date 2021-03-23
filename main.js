3c028da3f619811e2c1c0cf3f8f9b7a8d5a9457e
nacer <nacer.yeles@gmail.com>
2021-03-23 11:55:22 +0100

    ✏️✏️✏️✏️✏️✏️✏️✏️toto =)

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let title = document.querySelector("h1");
+let title1 = document.querySelector("h2");
+let title2 = document.querySelector("h3");
 let p = document.querySelectorAll("p");
 
 title.innerHTML = "toto"
+title1.innerHTML = "toto"
+title2.innerHTML = "toto"
 
 title.style.backgroundColor = "yellow"
 
